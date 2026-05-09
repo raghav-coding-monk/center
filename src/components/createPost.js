0e3707ed2b36371c9a28b7c147dcef90af306d0c
raghav <emailofraghav@gmail.com>
2026-05-09 09:01:35 +0530

    new updated 2 commit for center 3

--- src/components/createPost.js
+++ src/components/createPost.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import Axios from "axios"
 const CreatePost = (props) => {
